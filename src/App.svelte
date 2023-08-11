c5a5c1288138d54d9ae03de19c59cd8815e9bd55
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-11 13:26:50 +0800

    [UPDATE] Add card list component

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,26 @@
 <script>
-	import TodoCard from "./TodoCard.svelte";
+  import CardList from "./CardList.svelte";
 
-	let cards = [
+
+	let taskCards = [
 		{todo: 'Belajar HTML', list: 'Tasks'},
 		{todo: 'Belajar CSS Framework Bulma', list: 'Tasks'},
 		{todo: 'Belajar Javascript', list: 'Tasks'},
 		{todo: 'Belajar Svelte', list: 'Tasks'},
 	];
 
-	let todo = "";
+	let inProgressCards = [];
+	let doneCards = [];
 
-	function handlerAddCard() {
-		// DOM element dari data cards tidak akan terupdate jika tidak menggunakan operator assigment "="
-		// Misal cards.push() tampilan list card tidak akan ter update
-
-		cards = [...cards, {todo, list: 'Tasks'}];
-		todo = "";
-	}
-
-	function onInputEnter(e) {
-		if(e.key == "Enter"){
-			handlerAddCard();
-		}
-	}
 </script>
 
 <div class="container is-fullhd is-fluid">
 	<h1 class="is-size-3">TODO APP</h1>
 
 	<div class="columns">
-		<div class="column is-4">
-			<div class="card has-background-light">
-				<div class="card-header">
-					<div class="card-header-title">Task</div>
-				</div>
-				<div class="card-content">
-					{#each cards as card}
-						<TodoCard content={card.todo} />
-					{/each}
-					<input type="text" class="input is-primary mb-1" bind:value={todo} on:keydown={onInputEnter}/>
-					<button on:click={handlerAddCard} class="button is-primary">Add Card</button>
-				</div>
-			</div>
-		</div>
-
+		<CardList cards={taskCards} listName={'Task'} />
+		<CardList cards={inProgressCards} listName={'In Progress'} />
+		<CardList cards={doneCards} listName={'Done'} />
 	</div>
 </div>
 
---
+++ src/CardList.svelte
@@ -0,0 +1,39 @@
+<script>
+    import TodoCard from "./TodoCard.svelte";
+
+    // Mendefinisikan props
+    export let cards, listName;
+
+    let todo = "";
+
+    function handlerAddCard() {
+        // DOM element dari data cards tidak akan terupdate jika tidak menggunakan operator assigment "="
+        // Misal cards.push() tampilan list card tidak akan ter update
+
+        cards = [...cards, {todo, list: 'Tasks'}];
+        todo = "";
+    }
+
+    function onInputEnter(e) {
+        if(e.key == "Enter"){
+            handlerAddCard();
+        }
+    }
+</script>
+
+<style></style>
+
+<div class="column is-4">
+    <div class="card has-background-light">
+        <div class="card-header">
+            <div class="card-header-title">{listName}</div>
+        </div>
+        <div class="card-content">
+            {#each cards as card}
+                <TodoCard content={card.todo} />
+            {/each}
+            <input type="text" class="input is-primary mb-1" bind:value={todo} on:keydown={onInputEnter}/>
+            <button on:click={handlerAddCard} class="button is-primary">Add Card</button>
+        </div>
+    </div>
+</div>
